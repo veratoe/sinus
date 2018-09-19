7a11431cc01e9a9094975f2d449325ad58f20a04
Boyke <veratoe@gmail.com>
2018-09-19 21:34:29 +0200

    wubbels

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
 var canvas = document.querySelector("canvas");
 var ctx = canvas.getContext("2d");
-
-
-canvas.width = innerWidth;
-canvas.height = innerHeight;
-
 var imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
 
 var lx, ly; 
